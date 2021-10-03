6e70db6a3d712bd3556b563f4dbe86477d728b75
M Brammall <mibrammall.github@gmail.com>
2021-10-03 21:42:16 +0100

    Annotations to the max val program

--- chapter03/max_value/max.s
+++ chapter03/max_value/max.s
@@ -8,20 +8,43 @@
     .globl _start
 
 _start:
+# Move the value of 0 into the index register
     movl $0, %edi
+# Read 4 bytes from the value of the index register
+# Move this value into the eax register
     movl data_items(,%edi,4), %eax
+# This is the biggest value since we haven't started the
+# loop
     movl %eax,%ebx
 
+# We fall into here without having to start it
 start_loop:
+# We don't really have a loop, we're just iterating
+# through memory until we hit a 0.
+# Compare the value in the eax register to 0
     cmpl $0, %eax
+# If equal, go to loop exit
     je loop_exit
+# Increment the value in the index register
     incl %edi
+# Read the next 4 bytes from the data_items
+# Put it into the eax register.
     movl data_items(,%edi,4), %eax
+# Compare the value in ebx (the maximum)
+# to the value in eax (the current)
     cmpl %ebx, %eax
+# If eax < ebx, it's not the max, so go on 
+# to the next value
     jle start_loop
+# Otherwise it's the biggest we have found so far,
+# so put it into the ebx register
     movl %eax, %ebx
+# On to the next value
     jmp start_loop
 
 loop_exit:
+# Load the number 1 into eax, which is the number for the exit
+# system call
     movl $1, %eax
+# Interrupt with a system call
     int $0x80
